899976a78b806288fe8a5deb08efc8a38a861950
Diego Ramos Claumann <dclaumanndeveloper@protonmail.com>
2024-08-20 19:06:43 -0300

    updated

--- organizer.py
+++ organizer.py
@@ -16,11 +16,11 @@ def organizar_arquivo(diretorio,ano):
             os.mkdir(tipo)
     for arquivo in lista_arquivos:
         pasta_destino = arquivo.split(".")[-1]
-        timestamp = os.path.getmtime(arquivo)
-        dataarquivo = datetime.fromtimestamp(timestamp).strftime('%Y')
-        if(int(dataarquivo) < ano):
-            os.remove(arquivo)
-            continue
+        #timestamp = os.path.getmtime(arquivo)
+        #dataarquivo = datetime.fromtimestamp(timestamp).strftime('%Y')
+        #if(int(dataarquivo) < ano):
+        #    os.remove(arquivo)
+        #    continue
         de = os.path.join(os.getcwd(),arquivo)
         para = os.path.join(os.getcwd(), pasta_destino, arquivo)
         os.replace(de,para)
